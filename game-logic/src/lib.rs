ae3cc345a06d8b826786402de57f3f6c717354be
erer1243 <erer1243@users.noreply.github.com>
2024-05-28 22:49:03 -0400

    Progress

--- game-logic/src/lib.rs
+++ game-logic/src/lib.rs
@@ -1,48 +1,25 @@
 #![allow(unused)]
 
 //! Sources:
-//! https://hasbro-new.custhelp.com/app/answers/detail/a_id/55/~/what-is-the-total-face-value-of-all-the-scrabble-tiles%3F
-//! https://hasbro-new.custhelp.com/app/answers/detail/a_id/19/related/1
-//! https://i.pinimg.com/originals/9a/91/ab/9a91abcf38624a17c3b158a56eaa7e84.jpg
-//! https://github.com/dwyl/english-words
+//! - <https://hasbro-new.custhelp.com/app/answers/detail/a_id/55/~/what-is-the-total-face-value-of-all-the-scrabble-tiles%3F>
+//! - <https://hasbro-new.custhelp.com/app/answers/detail/a_id/19/related/1>
+//! - <https://i.pinimg.com/originals/9a/91/ab/9a91abcf38624a17c3b158a56eaa7e84.jpg>
+//! - <https://github.com/dwyl/english-words>
 
+use rand::seq::SliceRandom;
 use std::{
     fmt::Display,
     fs::File,
     io::{self, BufRead, BufReader},
-    ops::Index,
+    ops::{Index, IndexMut},
+    sync::Mutex,
 };
 
+#[rustfmt::skip]
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 #[repr(u8)]
 pub enum Tile {
-    A = 0,
-    B,
-    C,
-    D,
-    E,
-    F,
-    G,
-    H,
-    I,
-    J,
-    K,
-    L,
-    M,
-    N,
-    O,
-    P,
-    Q,
-    R,
-    S,
-    T,
-    U,
-    V,
-    W,
-    X,
-    Y,
-    Z,
-    Blank = 26,
+    A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, Blank,
 }
 
 impl Tile {
@@ -51,6 +28,14 @@ impl Tile {
             unsafe { Some(std::mem::transmute(n)) }
         } else {
             None
+        }
+    }
+
+    fn as_char(self) -> char {
+        if self == Tile::Blank {
+            '*'
+        } else {
+            char::from(b'A' + self as u8)
         }
     }
 
@@ -98,12 +83,7 @@ impl Tile {
 
 impl Display for Tile {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if *self == Tile::Blank {
-            write!(f, "*")
-        } else {
-            let ch = char::from(b'A' + *self as u8);
-            write!(f, "{ch}")
-        }
+        write!(f, "{}", self.as_char())
     }
 }
 
@@ -118,17 +98,17 @@ enum Modifier {
 #[derive(Clone, Copy, Debug, Default)]
 pub struct Board([[Option<Tile>; 15]; 15]);
 
-impl std::ops::Deref for Board {
-    type Target = [[Option<Tile>; 15]; 15];
+impl Index<usize> for Board {
+    type Output = [Option<Tile>; 15];
 
-    fn deref(&self) -> &Self::Target {
-        &self.0
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
     }
 }
 
-impl std::ops::DerefMut for Board {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
+impl IndexMut<usize> for Board {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.0[index]
     }
 }
 
@@ -165,42 +145,65 @@ struct Game {
 }
 
 impl Game {
-    fn new(num_players: u8) -> Self {
+    fn new(num_players: u8, randomize: bool) -> Self {
         assert!(num_players > 1 && num_players <= 4);
 
-        let board = Board::default();
-        let tile_bag = Vec::from_iter(Tile::iter_game_count());
-        let players = vec![Player::default(); num_players as usize];
-        let whose_turn = 0;
+        let mut tile_bag = Vec::from_iter(Tile::iter_game_count());
+        if randomize {
+            tile_bag.shuffle(&mut rand::thread_rng());
+        } else {
+            tile_bag.shuffle(&mut rand::rngs::mock::StepRng::new(0, 1));
+        }
 
         Self {
-            players,
-            board,
+            players: vec![Player::default(); num_players as usize],
+            board: Board::default(),
             tile_bag,
-            whose_turn,
+            whose_turn: 0,
         }
+    }
+
+    pub fn whose_turn(&self) -> u8 {
+        self.whose_turn
     }
 }
 
-#[derive(Debug)]
-pub struct WordList {
-    words: Vec<String>,
+impl Display for Game {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        fn tiles_str(ts: &[Tile]) -> String {
+            ts.iter().map(Tile::to_string).collect::<Vec<_>>().join(" ")
+        }
+        writeln!(f, "{}", self.board)?;
+        for (n, p) in self.players.iter().enumerate() {
+            let tiles = tiles_str(&p.tiles);
+            writeln!(f, "Player {n} | {} points | [{}]", p.score, tiles)?;
+        }
+        writeln!(f, "It is player {}'s turn", self.whose_turn)?;
+        writeln!(f, "Remaining tiles: {}", tiles_str(&self.tile_bag))?;
+        Ok(())
+    }
 }
 
-impl WordList {
-    pub fn load() -> io::Result<Self> {
-        let f = File::open("words.txt")?;
+static WORDLIST: Mutex<Vec<String>> = Mutex::new(Vec::new());
+
+pub fn load_wordlist() -> io::Result<()> {
+    let mut wl = WORDLIST.lock().unwrap();
+    if wl.is_empty() {
+        let f = File::open("words.txt").or_else(|_| File::open("game-logic/words.txt"))?;
         let rdr = BufReader::new(f);
         let mut words = rdr.lines().collect::<io::Result<Vec<String>>>()?;
 
         // This should be O(n) because the word list is already sorted, but just in case
         // there is an error somewhere.
         words.sort_unstable();
-
-        Ok(WordList { words })
+        *wl = words;
     }
 
-    pub fn check(&self, word: &str) -> bool {
-        self.words.binary_search(&word.to_string()).is_ok()
-    }
+    Ok(())
+}
+
+pub fn is_word(s: &String) -> bool {
+    load_wordlist().unwarp();
+    let wl = WORDLIST.lock().unwrap();
+    wl.binary_search(s).is_ok()
 }
