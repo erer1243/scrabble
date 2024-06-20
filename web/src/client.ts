b3ceb65b054f6b744e9ee0681a841712082a3e25
erer1243 <erer1243@users.noreply.github.com>
2024-06-20 01:02:33 -0400

    cleanup

--- web/src/client.ts
+++ web/src/client.ts
@@ -1,6 +1,7 @@
 import { GameT, InvalidMoveT, MoveT } from "./game-types"
 
 export const serverAddr: string = "ws://127.0.0.1:2222/"
+// export const serverAddr: string = "ws://146.115.147.29:2222/"
 
 export type TableT = {
   game: GameT
